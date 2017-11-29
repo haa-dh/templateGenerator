f0efd4655dc368c675ec528837a3e9c898b81145
haa-dh <danile.horsch@hoglundanalytics.com>
2017-11-29 16:05:21 -0600

    added parts array in javascript

--- js/generate.js
+++ js/generate.js
@@ -1,5 +1,12 @@
 // this is the array of page parts, it must match the partsArray.php array in order for the user to select the part
-var parts = ['qs'];
+
+
+var arr = ["hello", "no", "yes"];
+var options;
+for (var i = 0, len = arr.length; i < len; i++) {
+  options += "<option value="+arr[i]+">"+arr[i]+ "</option>";
+}
+
 
 // When the user selects the number of columns, show that number of input options
 $(document).ready(function() {
@@ -10,7 +17,7 @@ $(document).ready(function() {
     var columnNumber = $(this).val();
     for(var i = 0; i< columnNumber; i++) {
      $(this).parent().append(
-       "<h3>Column "+ (i+1) + " content</h3><select name='" + $(this).parent().attr('id') + (i+1) + "'><option>Hello</option></select>"
+       "<h3>Column "+ (i+1) + " content</h3><select name='" + $(this).parent().attr('id') + (i+1) + "'>"+options+"</select>"
      );
      // alert($(this).parent().attr('id') + i);
    }
